bd9db677e7d1f7bb3dbf261f99191cbbc375a259
Deniz Beşli <deniz.besli@outlook.com>
2021-01-29 00:49:56 +0300

    29 Ocak

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items; //dizi
+        //constructor: Bir class newlendiğinde çalışan bloga denir.
+        public MyList()
+        {
+            items = new T[0];  
+        }
+        public void Add (T item)
+        {
+            T[] tempArray = items; //Referans numarası newlediğimiz zaman silinmesin diye tempArray (geçiçidizi) değeri geçicidiziye tanımlarız.
+            items = new T[items.Length+1]; //1 arttırma işlemi
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Deniz", "Burak", "Yasemin", "Hakan" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler2.Add("ilker");
+            //Console.WriteLine(isimler2[4]);
+            //Console.WriteLine(isimler2[0]);
+
+            //List<string> isimler2 = new List<string>(); 1. kullanım
+            //isimler2.Add("Deniz");
+
+            List<string> isimler2 = new List<string> { "Deniz", "Burak","Yasemin", "Hakan"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+            //Koleksiyonlarda arrayler gibi değerlerini kaybetmeyecek alt yapı var. İş hayatında array fazla kullanılmaz onun yerine list kullanılır.
+
+
+
+        }
+    }
+}
